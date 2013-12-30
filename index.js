b121bd151086a91ce91118a68e9dfc9a220fc6c7
Jordan Harband <ljharb@gmail.com>
2013-12-30 02:43:11 -0800

    Implementation.

---
+++ index.js
@@ -0,0 +1,18 @@
+var Promise = require('promise');
+
+var Deferred = function Deferred() {
+	"use strict";
+
+	if (!(this instanceof Deferred)) { return new Deferred(); }
+
+	var self = this;
+	self.promise = new Promise(function (resolve, reject) {
+		self.resolve = resolve;
+		self.reject = reject;
+	});
+	return self;
+};
+Deferred.Promise = Promise;
+
+module.exports = Deferred;
+
